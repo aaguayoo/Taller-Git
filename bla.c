54c031f34a218c25db150d49cdb87c449356498d
Alejandro Aguayo-Ortiz <aaguayoo92@ciencias.unam.mx>
2020-03-11 12:56:45 -0600

    Se agrego bla

---
+++ bla.c
@@ -0,0 +1,23 @@
+/*!
+ * @file bla.c
+ *
+ * @brief 
+ *
+ * @author Alejandro Aguayo-Ortiz
+ *
+ * E-mail: aaguayoo92@ciencias.unam.mx
+ *
+ * @date 11-03-2020 - 12:56:41
+ *
+ * Created on: 11-03-2020 - 12:56:40
+ */
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<omp.h>
+#include<math.h>
+
+int main(int argc, char *argv[])
+{
+}
