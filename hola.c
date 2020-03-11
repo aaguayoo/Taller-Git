2d7d576d815b6bf3bd7bf98b43bc04c0479bce28
Alejandro Aguayo-Ortiz <aaguayoo92@ciencias.unam.mx>
2020-03-11 12:19:20 -0600

    Se agregó el archivo hola.c

---
+++ hola.c
@@ -0,0 +1,23 @@
+/*!
+ * @file hola.c
+ *
+ * @brief 
+ *
+ * @author Alejandro Aguayo-Ortiz
+ *
+ * E-mail: aaguayoo92@ciencias.unam.mx
+ *
+ * @date 11-03-2020 - 12:17:40
+ *
+ * Created on: 11-03-2020 - 12:17:37
+ */
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<omp.h>
+#include<math.h>
+
+int main(int argc, char *argv[])
+{
+}
